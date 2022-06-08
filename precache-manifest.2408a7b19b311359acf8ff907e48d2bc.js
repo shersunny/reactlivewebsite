08c9daaabb4e7cf8349019d42c3320130b213538
Sunny Kumar <shersunny01@gmail.com>
2022-06-09 00:55:09 +0530

    Updates

---
+++ precache-manifest.2408a7b19b311359acf8ff907e48d2bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2f1ff6cbb6834c549091fc4064f424",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "e2341f22544cf48e4464",
+    "url": "/reactlivewebsite/static/css/main.d0acee29.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/reactlivewebsite/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/reactlivewebsite/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2341f22544cf48e4464",
+    "url": "/reactlivewebsite/static/js/main.bbe53a07.chunk.js"
+  },
+  {
+    "revision": "abb9d06cbdd7faf460d9",
+    "url": "/reactlivewebsite/static/js/runtime-main.870ecabe.js"
+  },
+  {
+    "revision": "498f620d577b9b1b5955b552cdc22840",
+    "url": "/reactlivewebsite/static/media/sunny_photo.498f620d.png"
+  }
+]);
